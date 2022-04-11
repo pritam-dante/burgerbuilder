b599570e182f46b0e17b8e89943d6c8486e09d79
pritam-dante <pritam.dante@gmail.com>
2022-04-12 01:08:27 +0530

    saa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React,{ Component } from React;
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
-
+        <Layout>
+          <p>Hello</p>
+          <h1>Haaa</h1>
+        </Layout>
       </div>
     );
   }
 }
 
-export default App
+export default App;
